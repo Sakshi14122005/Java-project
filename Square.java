7d3178613d9825f81eb7463182392ee4abece98e
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-07-15 18:28:34 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,12 @@
+package Square;
+
+public class Square {
+
+    public static void main(String[] args) 
+    {
+        int num = 5;  
+
+        System.out.println("Square: " + (num * num));
+        System.out.println("Cube: " + (num * num * num));
+    }
+}
