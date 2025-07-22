ca8daaa3c6483f955ceb6b56aab522cc246d96be
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-07-22 15:28:27 +0530

    Add files via upload

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+package scanner;
+
+public class Modulus {
+    public static void main(String[] args) 
+    {
+        int a = 10;
+        int b = 20;
+
+    
+        System.out.println("Modulus: " + (a % b));
+    }
+}
