e49028e3dd3c65b18c13aa268d2b2fcb1b1d4c1b
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-07-22 15:12:03 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,19 @@
+package scanner;
+
+public class Perimeter
+{
+
+	    public static void main(String[] args)
+	    {
+	        int length = 5;
+	       int width = 3;
+	       int area,perimeter;
+	       
+	       area = length * width;
+            System.out.println("Area: " + area);
+            
+            perimeter = 2 * (length + width);
+	        System.out.println("Perimeter: " + perimeter);
+	       
+	    }
+	}
