284e0bc88432ea392605a21ee9adbede8cbb1b1a
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-07-22 19:08:04 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class Relational {
+	
+		public static void main(String[] args)
+		{
+			
+			System.out.println(20<21);
+			System.out.println(20>21);
+			System.out.println(20>=21);
+			System.out.println(20<=21);
+			System.out.println(20==21);
+			System.out.println(20!=21);
+			
+		}
+
+	}
+
