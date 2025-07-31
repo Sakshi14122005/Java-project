9db8edde248dce80615f56e2d47e2efb1ba4511d
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-07-31 20:31:17 +0530

    Add files via upload

---
+++ Statement.java
@@ -0,0 +1,17 @@
+package ifelse;
+import java.util.Scanner;
+public class Statement {
+	
+
+
+	     public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Number");
+		int a = sc.nextInt();
+		if(a>0)
+			{
+			System.out.println("The Number is Positive");
+			}
+		sc.close();
+		}
+	}
