2b0c1f4984204ae60bf4e46599d506c7b79bfafe
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-05 20:27:16 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,18 @@
+package statement;
+
+public class IfElseStatement {
+	public static void main(String[] args)
+	{
+		int num=13;
+		if(num%2==0)
+		{
+			System.out.println("Evennumber");
+			
+		}
+		else
+		{
+			System.out.println("Odd number");
+		}
+	}
+
+}
