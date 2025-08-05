ac10f8b237b8bcb4d510841e705df4e0b1b2bdbe
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-05 20:28:48 +0530

    Add files via upload

---
+++ NestedIfStatement.java
@@ -0,0 +1,19 @@
+package statement;
+
+public class NestedIfStatement {
+	public static void main(String[] args)
+	{
+		int height=6;
+		if(height>=6)
+		{
+			if(height>7)
+			{
+				System.out.println("Eligible for PST exam");
+			
+			}
+		}
+		System.out.println("Invalid input");
+		
+	}
+
+}
