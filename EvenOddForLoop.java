24980b1d9ce3ecc90b691db8f22fedd631c929aa
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-06 14:14:10 +0530

    Add files via upload

---
+++ EvenOddForLoop.java
@@ -0,0 +1,20 @@
+package Loop;
+
+public class EvenOddForLoop {
+	
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("even no is :"+i);
+				
+			}
+			else
+			{
+				System.out.println("odd no is:"+i);
+			}
+		}
+	}
+	}
