1829eaf8bc68aedf2072e2a389f1932b08c9cd86
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-07 20:01:20 +0530

    Add files via upload

---
+++ ReverseNumberLoop.java
@@ -0,0 +1,19 @@
+package Loop;
+
+public class ReverseNumberLoop {
+	public static void main(String[] args)
+	{
+		int n=521;
+		int s=0;
+		int t;
+		while(n>0)
+		{
+			t=n%10;
+			s=s*10+t;
+			n=n/10;
+		}
+		System.out.println(s);
+	}
+	
+
+}
