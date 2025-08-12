0ab6db81e6fa461b2ae636661cacc869840d3d94
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 20:47:15 +0530

    Add files via upload

---
+++ IntDataType.java
@@ -0,0 +1,15 @@
+package intDataType;
+
+public class IntDataType {
+	    public static void main(String[] args) {
+	
+	          int cityPopulation = 5000000;
+	          int districtPopulation = 25000;
+
+	           System.out.println("City Population: " +cityPopulation);
+	           System.out.println("District Population: " +districtPopulation);
+
+	    }
+	}
+
+
