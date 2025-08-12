21640aca3125316e86a29ad5ff0f4583902b7b03
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 21:38:52 +0530

    Add files via upload

---
+++ FloatDataType.java
@@ -0,0 +1,15 @@
+package FloatDataType;
+
+public class FloatDataType {
+
+	    public static void main(String[] args) {
+	   
+	        float currentRating = 4.7f;  
+	        float maxRating = 5.0f;  
+
+	        System.out.println("Current Video Rating: " + currentRating);
+	        System.out.println("Maximum Possible Rating: " + maxRating);
+	    }
+	}
+
+
