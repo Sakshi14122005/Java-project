8748772097653f32ec0c107b9a3ba8a3a16f5d13
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 20:28:59 +0530

    Add files via upload

---
+++ ShortDataType.java
@@ -0,0 +1,17 @@
+package shortDataType;
+
+public class ShortDataType {
+	public static void main(String[] args)
+	{
+		
+	        short regionOneIntensity = 12000;
+	        short regionTwoIntensity = -5000;
+	        short regionThreeIntensity = 0;
+
+	          System.out.println("Region One Intensity: " + regionOneIntensity);
+	          System.out.println("Region Two Intensity: " + regionTwoIntensity);
+	          System.out.println("Region Three Intensity: " + regionThreeIntensity);
+	    }
+
+
+}
