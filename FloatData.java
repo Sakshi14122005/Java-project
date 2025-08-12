071e669f1d87eadaa81cf034e7605ae46e347234
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 22:13:29 +0530

    Add files via upload

---
+++ FloatData.java
@@ -0,0 +1,15 @@
+package FloatDataType;
+
+public class FloatData {
+	
+	    public static void main(String[] args) {
+	
+	       float fuelEfficiency = 18.5f;
+	       float distanceCovered = 150f;
+
+	         System.out.println("Fuel Efficiency: " +fuelEfficiency +"km/1");
+	         System.out.println("Distance Covered: " +distanceCovered +"km");
+	  
+	    }
+	}
+
