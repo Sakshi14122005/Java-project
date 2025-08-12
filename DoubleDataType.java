d1229c87577b8d7c221e9e0529c07170b6f53a83
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 22:22:08 +0530

    Add files via upload

---
+++ DoubleDataType.java
@@ -0,0 +1,14 @@
+package DoubleDataType;
+
+public class DoubleDataType {
+	
+	    public static void main(String[] args) {
+	          double temperature = 28.3;
+	           double rainfallLevel = 125.6789;
+
+	            System.out.println("Temperature: " + temperature +"0C");
+	            System.out.println("Rainfall Level: " + rainfallLevel +"mm");
+
+	    }
+	}
+
