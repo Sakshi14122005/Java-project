597ce34afb946eb57e8c96a241988a8b9494c497
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 20:53:30 +0530

    Add files via upload

---
+++ LongData.java
@@ -0,0 +1,14 @@
+package LongDataType;
+
+public class LongData {
+
+		    public static void main(String[] args){
+		        // Cook your dish here 
+		        long transactionId = 5_123_456_78_9000L;
+		        long branchCode = 3052;
+		        
+		        System.out.println("Transaction ID: " + transactionId);
+		        System.out.println("Barnch Code: " + branchCode);
+		    }
+		}
+
