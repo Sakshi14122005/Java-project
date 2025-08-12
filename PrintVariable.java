2efa307eb9743b35bdb319bf712cb4add3fe0889
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 20:15:35 +0530

    Add files via upload

---
+++ PrintVariable.java
@@ -0,0 +1,12 @@
+package printusingvariable;
+
+public class PrintVariable {
+	
+		    public static void main(String[] args){
+		        // Cook your dish here 
+		        int value=18;
+		        System.out.println("The value stored in variable is"+value);
+		    }
+		}
+
+
