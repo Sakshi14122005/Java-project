07bf178dc97940fbf07803dd7abdb20d01a3eb65
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-12 20:16:16 +0530

    Add files via upload

---
+++ TemperatureSensorReadings.java
@@ -0,0 +1,13 @@
+package printusingvariable;
+
+public class TemperatureSensorReadings {
+    public static void main(String[] args) {
+        // Declare and assign temperature readings
+        byte hotTemp = 45;
+        byte coldTemp = -20;
+
+        // Print the temperature readings
+        System.out.println("Hot Region Temperature: " + hotTemp);
+        System.out.println("Cold Region Temperature: " + coldTemp);
+    }
+}
