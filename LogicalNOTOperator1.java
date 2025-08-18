8be89f9eb64d3d76942629e4fed0ef731299f55f
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:29:40 +0530

    Add files via upload

---
+++ LogicalNOTOperator1.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class LogicalNOTOperator1 {
+	
+	    public static void main(String[] args) {
+	       
+	        int marks = 40;
+
+	          boolean didNotPass = !(marks >= 50);
+
+
+	          System.out.println("Did the student NOT pass? " + didNotPass);
+	    }
+	}
+
