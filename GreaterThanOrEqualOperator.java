acfead6726d16393b2bbf8d4bab85599a1dadbfc
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:00:56 +0530

    Add files via upload

---
+++ GreaterThanOrEqualOperator.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class GreaterThanOrEqualOperator {
+	
+	    public static void main(String[] args) {
+	        int passingMarks = 40;  // Minimum marks required to pass
+	        int studentMarks = 45;  // Marks obtained by the student
+
+	        boolean hasPassed = studentMarks >= passingMarks; // Store result in variable
+
+	        System.out.println("Has the student passed? " + hasPassed);
+	    }
+	}
+
+
+
