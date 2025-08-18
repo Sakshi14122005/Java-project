aa2905e3a0ce4cebc3a22f4fb78e247b609664ea
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:27:52 +0530

    Add files via upload

---
+++ LogicalANDOperator1.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class LogicalANDOperator1 {
+	
+	    public static void main(String[] args) {
+	    
+	        int age = 15;
+
+	        boolean hasTicket = true;
+
+	        boolean isEligible = age >=13 && hasTicket;
+
+	        System.out.println("Age: " +age);
+
+	        System.out.println("Has Ticket: " + hasTicket);
+
+
+	        System.out.println("Is Eligible for Movie Night: " + isEligible);
+	    }
+	}
+
+
+
+
