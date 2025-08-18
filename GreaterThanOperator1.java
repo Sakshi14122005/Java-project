f93d572e191582909daa6fe928bd1bf3492cc0db
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:40:51 +0530

    Add files via upload

---
+++ GreaterThanOperator1.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class GreaterThanOperator1 {
+
+	    public static void main(String[] args) {
+	        // Boiling point of water in Celsius
+	        final int BOILING_POINT = 100;
+	        
+	        // Current water temperature (you can change this value to test)
+	        int waterTemperature = 105;
+	        
+	        // Check if water is boiling
+	        boolean isBoiling = true; // Add your code here
+	        
+	        // Print the result
+	        System.out.println("Water temperature: " + waterTemperature + "°C");
+	        System.out.println("Boiling point: " + BOILING_POINT + "°C");
+	        System.out.println("Is the water boiling? " + isBoiling);
+	    }
+	}
+
