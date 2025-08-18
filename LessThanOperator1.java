6cb80a532222f553efa2e26a41154f0776995361
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:41:28 +0530

    Add files via upload

---
+++ LessThanOperator1.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class LessThanOperator1 {
+	
+	    public static void main(String[] args) {
+	        int lowBatteryLevel = 20;  // Minimum battery level
+	        int currentBattery = 15;   // Current battery percentage
+	        
+	        boolean isBatterLow =   true;// Add code here to compare
+	        
+	        // Write statement to print the result
+	        System.out.println("Battery low?" + isBatterLow);
+	    }
+	}
+
