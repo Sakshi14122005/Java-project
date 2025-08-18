e5719cb9a90bb938b594c5d636ad114f86c84861
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:40:37 +0530

    Add files via upload

---
+++ GreaterThanOperator.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class GreaterThanOperator {
+	
+	    public static void main(String[] args) {
+	        int speedLimit = 60;  // Speed limit in km/h
+	        int carSpeed = 70;    // Current speed of the car
+
+	        System.out.println("Over speed? " + (carSpeed > speedLimit));
+	    }
+	}
+
+
