770b733db7b7cc38f4109901a8e60cbfcb3926c0
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:41:59 +0530

    Add files via upload

---
+++ NotEqualToOperator1.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class NotEqualToOperator1 {
+	
+	    public static void main(String[] args) {
+	        // Total working days in a month
+	        int totalWorkingDays = 22;
+
+	        // Days attended by an employee
+	        int daysAttended = 20;
+
+	        // Complete the comparision
+	        boolean isAttendanceIncomplete = true;
+
+	        // Printing the result directly
+	        System.out.println("Attendance incomplete: " + isAttendanceIncomplete);
+
+	    }
+	}
+
+
+
