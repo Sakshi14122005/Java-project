b960712bdd3a22104e0c1414a8d27d375cdf9338
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:27:30 +0530

    Add files via upload

---
+++ LogicalANDOperator.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class LogicalANDOperator {
+
+	    public static void main(String[] args) {
+	       
+	        boolean attendedMorningClass = true;
+
+	        
+	        boolean attendedAfternoonClass = true;
+
+	        
+	        boolean fullDayAttendance = attendedMorningClass && attendedAfternoonClass;
+
+	        
+	        System.out.println("Full Day Attendance: " + fullDayAttendance); // Output: true
+	    }
+	}
+
+
+
