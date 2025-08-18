66600fe88eb925f34a989d89d958f0c7255124ec
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:29:14 +0530

    Add files via upload

---
+++ LogicalNOTOperator.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class LogicalNOTOperator {
+	
+	    public static void main(String[] args) {
+	     
+	        boolean isDoorLocked = true;
+
+	      
+	        boolean isDoorUnlocked = !isDoorLocked;
+
+	     
+	        System.out.println("Is the door unlocked? " + isDoorUnlocked);
+	    }
+	}
+
+
