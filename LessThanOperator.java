b177427468d736a03000b9ae7ea9c865020af6f3
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:41:09 +0530

    Add files via upload

---
+++ LessThanOperator.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class LessThanOperator {
+
+	    public static void main(String[] args) {
+	        int minTemperature = 10;  // Minimum safe temperature
+	        int currentTemperature = 5; // Current temperature
+
+	        System.out.println("Is temperature too low? " + (currentTemperature < minTemperature));
+	    }
+	}
+
