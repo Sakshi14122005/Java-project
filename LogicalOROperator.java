3df9130a856fbb12e561504a25d791bdd741a9ea
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:28:36 +0530

    Add files via upload

---
+++ LogicalOROperator.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class LogicalOROperator {
+
+	    public static void main(String[] args) {
+	      
+	        boolean isSaturday = true;
+
+	        boolean isSunday = false;
+
+	        boolean isWeekend = isSaturday || isSunday;
+
+	        System.out.println("Is it the weekend? " + isWeekend); 
+	    }
+	}
+
+
