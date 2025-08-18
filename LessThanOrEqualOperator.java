3bc478413cb8c957bda8713856bd20f638b467bf
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:00:06 +0530

    Add files via upload

---
+++ LessThanOrEqualOperator.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class LessThanOrEqualOperator {
+	
+	    public static void main(String[] args) {
+	        int speedLimit = 60;  // Maximum allowed speed
+	        int vehicleSpeed = 55; // Current vehicle speed
+
+	        boolean isWithinLimit = vehicleSpeed <= speedLimit; // Check if within speed limit
+
+	        System.out.println("Is the vehicle within the speed limit? " + isWithinLimit);
+	    }
+	}
+
