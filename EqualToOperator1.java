cce943d6f1351b518bc7e6c24d0ed2b05d75efc7
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 18:40:20 +0530

    Add files via upload

---
+++ EqualToOperator1.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class EqualToOperator1 {
+	
+	    public static void main(String[] args) {
+	        // Comparing Student Marks (primitive comparison)
+	        int student1Marks = 85;
+	        int student2Marks = 85;
+	        
+	        // write your code below 
+	        boolean isMarksEqual = true ;
+	        
+	        System.out.println("Does student1 have the same marks as student2? " + isMarksEqual); // true
+
+	    }
+	}
+
+
