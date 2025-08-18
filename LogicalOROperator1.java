77657e410200a964a61f53a838bbe27bbad2edc6
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-18 19:28:55 +0530

    Add files via upload

---
+++ LogicalOROperator1.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class LogicalOROperator1 {
+
+	    public static void main(String[] args) {
+	      
+	        int creditScore = 650;
+
+	
+	        double monthlyIncome = 55000;
+
+	      
+	        boolean isEligible = creditScore >=700 || monthlyIncome >= 50000;
+
+
+	        System.out.println("Eligible for Loan: " + isEligible);
+	    }
+	}
+
+
