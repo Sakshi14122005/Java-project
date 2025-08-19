10c564ea86e4cac7f3cbdf87f18da3844eecd52e
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-19 16:27:05 +0530

    Add files via upload

---
+++ CompandLogicalOperator.java
@@ -0,0 +1,16 @@
+package Operator;
+
+public class CompandLogicalOperator {
+
+	    public static void main(String[] args) {
+	
+	        int a = 12, b = 4, c = 2;
+	        boolean result;
+
+	        result = (a > b) && (b < c) || (a % c == 0); 
+
+	        System.out.println(result); 
+	    }
+	}
+
+
