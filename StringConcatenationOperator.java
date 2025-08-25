1ba22cab2fe15b6e88aea4f5417a043b20bc437e
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-25 22:06:12 +0530

    Add files via upload

---
+++ StringConcatenationOperator.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class StringConcatenationOperator {
+	 public static void main(String[] args) {
+	        // Declaring two string variables
+	        String firstName = "John";  // First part of the name
+	        String lastName = "Doe";    // Second part of the name
+
+	        // Concatenating the first name and last name with a space
+	        String fullName = firstName + " " + lastName;  // Using + operator to concatenate
+
+	        // Printing the full name
+	        System.out.println("Full Name: " + fullName);  // Output: John Doe
+	    }
+
+}
