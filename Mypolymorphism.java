b27bfc121439a2d59e0bb33a4426db0b97f1bade
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-08-25 18:41:06 +0530

    Add files via upload

---
+++ Mypolymorphism.java
@@ -0,0 +1,32 @@
+package Inheritance;
+
+class Animal888 {
+	public void animalSound() {
+		System.out.println("The animal makes a sound");
+	}
+}
+
+class Pig extends Animal888 {
+	public void animalSound() {
+		System.out.println("The pig says: wee wee");
+	}
+}
+
+class Dog888 extends Animal888 {
+	public void animalSound() {
+		System.out.println("The dog says: bow wow");
+	}
+}
+
+class Mypolymorphism {
+	public static void main(String[] args) {
+		Animal888 myAnimal = new Animal888();
+		
+		Animal888 myPig = new Pig();
+		Animal888 myDog = new Dog888();
+
+		myAnimal.animalSound();
+		myPig.animalSound();
+		myDog.animalSound();
+	}
+}
