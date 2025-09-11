afa25da6b4a344e5d37d1e9075f1bd045d63aa4a
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-11 18:52:39 +0530

    Add files via upload

--- SuperKeywordWithConstructor.java
+++ SuperKeywordWithConstructor.java
@@ -1,45 +1,44 @@
-package com.pqr;
+package code;
 
-class Teacher{
-	String TeacherName;
-	String TeacherSubName;
-	
-	Teacher()
-	{
-		System.out.println("I am default Constructor");
-	}
-	Teacher(String teacherName) 
-	{
-		this("Thakrey Sir","JAVA");
-		TeacherName = teacherName;
-		System.out.println(TeacherName);
-	}
-	Teacher(String teacherName, String teacherSubName) 
-	{
-		TeacherName = teacherName;
-		TeacherSubName = teacherSubName;
-		System.out.println(TeacherName);
-		System.out.println(TeacherSubName);
-		
-	}
+class Teacher {
+    String TeacherName;
+    String TeacherSubName;
+
+    Teacher() {
+        System.out.println("I am default Constructor");
+    }
+
+    Teacher(String teacherName) {
+        this("Thakrey Sir", "JAVA"); 
+        TeacherName = teacherName;
+        System.out.println(TeacherName);
+    }
+
+    Teacher(String teacherName, String teacherSubName) {
+        TeacherName = teacherName;
+        TeacherSubName = teacherSubName;
+        System.out.println(TeacherName);
+        System.out.println(TeacherSubName);
+    }
 }
-class Student extends Teacher{
-	int StudRoll;
-	String StudentName;
-	
-	Student()
-	{
-		super("Mande sir");
-	}
-    Student(int studRoll, String studentName) {
-		StudRoll = studRoll;
-		StudentName = studentName;
-		System.out.println(StudRoll+" and "+StudentName);
-	}
+
+class Student1 extends Teacher {
+    int StudRoll;
+    String StudentName;
+
+    Student1() {
+        super("Mande sir"); 
+    }
+
+    Student1(int studRoll, String studentName) {
+        StudRoll = studRoll;
+        StudentName = studentName;
+        System.out.println(StudRoll + " and " + StudentName);
+    }
 }
+
 public class SuperKeywordWithConstructor {
-	public static void main(String[] args) {
-		Student s1 = new Student();
-	}
-	
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();  
+    }
 }
