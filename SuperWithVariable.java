9d2decbd0647a81b0c1f044adcca2e5875ed32a7
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-11 18:48:20 +0530

    Add files via upload

--- SuperWithVariable.java
+++ SuperWithVariable.java
@@ -1,22 +1,21 @@
-package com.pqr;
-class Car
-{
-	String carName="I am a Car";
-}
-class Bolero extends Car
-{
-	String carName="I am Bolero";
-	void display()
-	{
-		System.out.println(this.carName);
-		System.out.println(super.carName);
-		}
-}
-public class SuperWithVariable {
-	public static void main(String[] args)
-	{
-		Bolero b1=new Bolero();
-		b1.display();
-	}
+package code;
 
+class Car {
+    String carName = "I am a Car";
+}
+
+class Bolero1 extends Car {
+    String carName = "I am Bolero";
+
+    void display() {
+        System.out.println(this.carName);   
+        System.out.println(super.carName);  
+    }
+}
+
+public class SuperWithVariable {
+    public static void main(String[] args) {
+        Bolero1 b1 = new Bolero1();  
+        b1.display();
+    }
 }
