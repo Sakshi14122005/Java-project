fe3b9bcc27597489e9ae95bbc91baa5d0117947e
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-11 18:29:15 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,30 +1,46 @@
-package saturday;
+package code;
+import java.util.Scanner;
+
+class StudentTest {
+    String name;
+    int age;
+    float grade;
+
+    // Constructor
+    StudentTest(String name, int age, float grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // Method to display student info
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
 
 public class Student {
-	String name;
-	int rollNo;
-	
-	static
-	{
-		System.out.println("I am student of SMBST");
-	}
-	Student(String n,int r)
-	{
-		name=n;
-		rollNo=r;
-	}
-	void display()
-	{
-		System.out.println("Ener a Name:-"+name);
-		System.out.println("Ener a rollNo:-"+rollNo);
-	}
-	public static void main(String[] args)
-	{
-		Student s=new Student("SAKSHI",01);
-		s.display();
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
 
+        System.out.print("Enter student age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter student grade: ");
+        float grade = sc.nextFloat();
+
+     
+        StudentTest s1 = new StudentTest(name, age, grade);  //create object using constructor
+
+      
+        System.out.println("\nStudent Information:");
+        s1.display();
+
+        sc.close();
+    }
 }
