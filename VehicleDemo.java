2579848de7c5e65ef65921d1d29cb4c810a33eb5
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-15 23:43:18 +1100

    Add files via upload

---
+++ VehicleDemo.java
@@ -0,0 +1,40 @@
+package code;
+interface Vehicle {
+    void start();
+    void stop();
+}
+
+class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+    @Override
+    public void stop() {
+        System.out.println("Car has stopped.");
+    }
+}
+
+class Bike implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Bike is starting...");
+    }
+    @Override
+    public void stop() {
+        System.out.println("Bike has stopped.");
+    }
+}
+
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle bike = new Bike();
+
+        car.start();
+        car.stop();
+
+        bike.start();
+        bike.stop();
+    }
+}
