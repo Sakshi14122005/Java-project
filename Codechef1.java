a42b828efcb6a498c0d840449485b721ec72a2a2
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-19 01:57:03 +1100

    Add files via upload

---
+++ Codechef1.java
@@ -0,0 +1,19 @@
+package codechef;
+
+public class Codechef1 {
+
+
+
+		public static void main(String[] args) {
+	        // Declare and initialize a string variable to store the file name
+	        String fileName = "picture.jpg";
+
+	        // Use the endsWith() method to check if the file name ends with ".jpg"
+	        System.out.println("Ends with '.jpg': " + fileName.endsWith(".jpg"));
+
+	        // Use the endsWith() method to check if the file name ends with ".png"
+	        System.out.println("Ends with '.png': " + fileName.endsWith(".png"));
+	    }
+	}
+
+
