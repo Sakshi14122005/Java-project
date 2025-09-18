57de70e470d9f17d68d6a656dbc95a18b4f02df6
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-19 01:57:26 +1100

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,16 @@
+package codechef;
+
+public class Codechef {
+
+		 public static void main(String[] args) {
+		      
+		        String originalString = "hello world";
+
+		       
+		        String upperString = originalString.toUpperCase();
+
+		    
+		        System.out.println("Uppercase Message: " + upperString);
+		    }
+	}
+
