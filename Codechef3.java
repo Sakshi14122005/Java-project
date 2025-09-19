bdef69ce792ad304f6563ccbdd9311455bdb807f
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-19 23:20:55 +1100

    Add files via upload

---
+++ Codechef3.java
@@ -0,0 +1,17 @@
+package codechef;
+
+public class Codechef3 {
+
+		 public static void main(String[] args) {
+		      
+		        String text = "Hello, World!";
+
+		        
+		        String result = text.substring(7);
+
+		       
+		        System.out.println("Extracted Substring: " + result);
+		    }
+	}
+
+
