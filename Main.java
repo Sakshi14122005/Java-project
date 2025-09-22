8ba7b1727f7247b96940cbf942ec2728badab920
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-22 19:46:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,50 @@
+package codechef;
+interface ProductInterface {
+}
 
-package Encapsulation;
-class BankAccount { 
-    private String accountHolder; 
-    private double balance; 
- 
-    public String getAccountHolder() { 
-        return accountHolder; 
-    } 
- 
-    public void setAccountHolder(String accountHolder) { 
-        this.accountHolder = accountHolder; 
-    } 
- 
-    public double getBalance() { 
-        return balance; 
-    } 
- 
-    public void setBalance(double balance) { 
-        if(balance >= 0) { 
-            this.balance = balance; 
-        } else { 
-            System.out.println("Balance cannot be negative!"); 
-        } 
-    } 
-} 
- 
-public class Main { 
-    public static void main(String[] args) { 
-        BankAccount acc = new BankAccount(); 
-        acc.setAccountHolder("Aditya"); 
-        acc.setBalance(5000); 
- 
-        System.out.println("Account Holder: " + acc.getAccountHolder()); 
-        System.out.println("Balance: ₹" + acc.getBalance()); 
- 
-        acc.setBalance(-1000); 
-    } 
-} 
- 
+class Product implements ProductInterface {
+	private int productId;
+	private String productName;
+	private double productCost;
+	private int productQuantity;
+	private static int objectCount = 0;
+
+	public Product() {
+		productId = 0;
+		productName = "NO name";
+		productCost = 0.0;
+		productQuantity = 0;
+		objectCount++;
+	}
+
+	public Product(int productId, String productName, double productCost, int productQuantity) {
+		this.productId = productId;
+		this.productName = productName;
+		this.productCost = productCost;
+		this.productQuantity = productQuantity;
+		objectCount++;
+	}
+
+	public void displayProduct() {
+		System.out.println("Product ID: " + productId);
+		System.out.println("Product Name: " + productName);
+		System.out.println("Product Cost: " + productCost);
+		System.out.println("Product Quantity: " + productQuantity);
+	}
+
+	public static int getObjectCount() {
+		return objectCount;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Product product1 = new Product();
+		Product product2 = new Product(101, "Laptop", 50000.0, 10);
+
+		product1.displayProduct();
+		product2.displayProduct();
+
+		System.out.println("Total Product Objects: " + Product.getObjectCount());
+	}
+}
