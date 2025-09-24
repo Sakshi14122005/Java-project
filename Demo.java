8163367a3c1b9a99ad70597ca7eb2728600eea8c
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-24 19:49:31 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,22 +1,56 @@
-package Product;
-import java.util.Scanner;
-public class Demo {
-	    public static void main(String[] args)
-	    {
-	        Scanner sc = new Scanner(System.in);
+package Codechef;
 
-	        System.out.print("Enter ID: ");
-	        int id = sc.nextInt(); 
-	        sc.nextLine();
+class Address
+{
+private int laneno;
+private int houseno;
+private String area;
+Address(int laneno, int houseno, String area)   
+{
+this.laneno=laneno;
+this.houseno=houseno;
+this.area=area;
+}
+public String toString()
+{
+return this.laneno+" " +this.houseno+" " +this.area+" ";
+}
+}
 
-	        System.out.print("Enter Name: ");
-	        String name = sc.nextLine();
 
-	        System.out.print("Enter Price: ");
-	        double price = sc.nextDouble();
-	        Product1 p = new Product1(id, name, price);
+class Student 
+{
+private int rollno;
+private String name;
+private Address add;  
+Student(int rollno, String name, Address add)
+{
+this.rollno=rollno;
+this.name=name;
+this.add=add;
+}
+public String toString()    
+{
+return this.rollno+" " + this.name+" " + add.toString();    
+}
+public Object Clone()  throws CloneNotSupportedException
+{
+return super.clone();
+}
+}
 
-	        sc.close();
-	    }
-	}
+
+class Demo 
+{
+public static void main(String args[])  throws CloneNotSupportedException 
+{
+Address address1=new Address(5,12,"Sangamner");
+Student s1=new Student(10,"Sakshi",address1);  
+System.out.println(s1); 
+{
+Student s2=(Student)s1.Clone();    
+System.out.println(s2);     
+}
+}
+}
 
