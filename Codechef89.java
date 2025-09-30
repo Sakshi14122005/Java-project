0ff379bbfa957658e633dfe4db51bf57f9c0a86a
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-30 15:43:02 +0530

    Add files via upload

---
+++ Codechef89.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef89 {
+	 public static void main(String[] args) {
+	      
+	        String message = "HELLO WORLD";
+
+	        String lowerMessage = message.toLowerCase();
+
+	    
+	        System.out.println("Lowercase Message: " + lowerMessage);
+	    }
+}
