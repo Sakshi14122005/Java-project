94e804bc29fa601beb7903595d6f09d90d643a46
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-30 15:42:44 +0530

    Add files via upload

---
+++ Codechef90.java
@@ -0,0 +1,15 @@
+package Codechef;
+
+public class Codechef90 {
+	 public static void main(String[] args) {
+	        
+	  
+	        String productName = "Samsung GALAXY Phone";
+	        
+	     
+	        String formattedProduct = productName.toLowerCase();
+	        
+	        
+	         System.out.println("Formatted  product Name: " +formattedProduct);
+	    }
+}
