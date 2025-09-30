2e2bdf2d9e3dd37821298831e8d5010f0397bbc7
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-30 15:43:18 +0530

    Add files via upload

---
+++ Codechef88.java
@@ -0,0 +1,19 @@
+package Codechef;
+
+public class Codechef88 {
+public static void main(String[] args) {
+        
+        String s1="Hello";
+        String s2="World";
+
+         s1 = s1.toUpperCase();
+         s2 = s2.toUpperCase();
+
+            String finalString =s1 + " " + s2;
+
+           System.out.println(finalString.length());
+
+           System.out.println(finalString);
+        
+    }
+}
