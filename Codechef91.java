0d7908f5b37d2faf1db4aa17d74d2944a8a50d8d
Sakshi14122005 <sakshihadwale08@gmail.com>
2025-09-30 15:42:29 +0530

    Add files via upload

---
+++ Codechef91.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef91 {
+	 public static void main(String[] args) {
+	   
+	        String message = "   Hello, World!   ";
+
+	     
+	        String trimmedMessage = message.trim();
+
+	 
+	        System.out.println("Trimmed Message: [" + trimmedMessage + "]");
+	    }
+}
